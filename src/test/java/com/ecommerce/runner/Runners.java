4519a6a8acab28dcd31fba9313ed3b60f9f1c40f
Masum Raza <56122183+masumrazait@users.noreply.github.com>
2024-09-03 23:58:18 +0530

    code added

--- src/test/java/com/ecommerce/runner/Runners.java
+++ src/test/java/com/ecommerce/runner/Runners.java
@@ -1,10 +1,11 @@
 package com.ecommerce.runner;
 
-public class Runners {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-	public static void main(String[] args) {
-		
-
-	}
+@CucumberOptions(features = "src/test/resources/features", glue = { "com.ecommerce.stepDefinitions" }, plugin = {
+		"html:target/cucucmber-html-report.html",
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" })
+public class Runners extends AbstractTestNGCucumberTests {
 
 }
